a4cfd03508c116c0904da619ff1196b460de1878
yahyasheriif <yahyasabdallah21@gmail.com>
2025-05-01 05:20:14 +0300

    done without styling

--- base/views.py
+++ base/views.py
@@ -1,6 +1,67 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView,FormView
+from django.urls import reverse_lazy
+from .models import Task
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-def taskList(request):
-    return HttpResponse("<h1>To Do List</h1>")
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    redirect_authenticated_user = True
+    form_class = UserCreationForm
+    success_url = reverse_lazy('tasks')
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+    ordering = ['complete']
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+        context['search_input'] = search_input
+        return context
+class TaskDetail(LoginRequiredMixin,DetailView):
+    model = Task
+    context_object_name = 'task'
+class TaskCreate(LoginRequiredMixin,CreateView):
+    model = Task
+    fields = ['title','description','complete']
+    success_url = reverse_lazy('tasks')
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin,UpdateView):
+    model = Task
+    context_object_name = 'task'
+    fields = ['title','description','complete']
+    success_url = reverse_lazy('tasks')
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskUpdate, self).form_valid(form)
+
+class TaskDelete(LoginRequiredMixin,DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGIN_URL = 'login'
